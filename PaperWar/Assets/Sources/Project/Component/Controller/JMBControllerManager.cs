e4451c36f11f6951bed58a88749d427321bb3cbd
han._.,Nari_ <luminous_crescent@naver.com>
2025-06-13 16:16:12 +0900

    feat : 이동 컴퍼넌트를 컨트롤러에서 사용.
    임시로 플레이어의 레거시 입력을 받고 WASD 이동하도록 구현.

--- PaperWar/Assets/Sources/Project/Component/Controller/JMBControllerManager.cs
+++ PaperWar/Assets/Sources/Project/Component/Controller/JMBControllerManager.cs
@@ -1,7 +1,44 @@
 using UnityEngine;
 
+
+
+[RequireComponent(typeof(JMBMovementManager))]
 public class JMBControllerManager : JMonoBehaviour
 {
     
-    
+    private JMBMovementManager movementComponent;
+
+
+    public override void OnConstructed()
+    {
+        base.OnConstructed();
+
+        doTickUpdate = true;
+        
+        movementComponent = GetComponent<JMBMovementManager>();
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        base.Tick(deltaTime);
+        
+        // 테스트용 임시 구현. 레거시 입력 시스템을 사용함.
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            movementComponent.InputMovementDirection(new Vector3(-1, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movementComponent.InputMovementDirection(new Vector3(1, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            movementComponent.InputMovementDirection(new Vector3(0, 0, 1));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movementComponent.InputMovementDirection(new Vector3(0, 0, -1));
+        }
+    }
 }
