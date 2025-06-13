22dbe1e90639748671eba6408bf9ca2eca0431cf
han._.,Nari_ <luminous_crescent@naver.com>
2025-06-13 15:23:12 +0900

    feat : 무브먼트 필수 핃드 추가.
    최고 이동 속력과이동 가속력, 현재 이동 속력 추가.

--- PaperWar/Assets/Sources/Project/Component/Movement/JMBMovementManager.cs
+++ PaperWar/Assets/Sources/Project/Component/Movement/JMBMovementManager.cs
@@ -1,7 +1,39 @@
 using UnityEngine;
 
+
 public class JMBMovementManager : JMonoBehaviour
 {
+
+    /**
+     * 최고 이동 속력. 현재 이동 속도가 가질 수 있는 최고값.
+     */
+    [SerializeField]
+    protected float maximumMovementSpeed;
+
+    /**
+     * 이동 가속력. 이동을 시작했을 때 이동 속력을 연속적으로 상승시키는 크기이다.
+     * 단위는 m/s.
+     */
+    [SerializeField]
+    protected float movementAcceleration;
+    
+    /**
+     * 현재 이동 속력. 현재 플레이어가 가진 속력.
+     * 틱에서 강체 속력이 이동 방향과 현재 이동 속력이 곱연산된 값으로 초기화 된다.
+     * 값의 범위는 0 보다 크거나 같고 maximumMovementSpeed 보다 작거나 같다.
+     */
+    protected float currentMovementSpeed;
+    
+    
+    public override void OnConstructed()
+    {
+        base.OnConstructed();
+        
+        doTickUpdate = true;
+
+        currentMovementSpeed = 0;
+    }
+    
     
     
 }
